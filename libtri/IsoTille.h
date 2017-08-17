98ae988222f7aa413e415639633d2b48da1c9944
transpixel <dave@transpixel.org>
2017-08-17 09:38:24 -0600

    libtri/IsoTille prototype skeleton code

---
+++ libtri/IsoTille.h
@@ -0,0 +1,370 @@
+//
+//
+// MIT License
+//
+// Copyright (c) 2017 Stellacore Corporation.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject
+// to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+// KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+// AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//
+
+#ifndef tri_IsoTille_INCL_
+#define tri_IsoTille_INCL_
+
+/*! \file
+\brief Declarations for tri::IsoTille
+*/
+
+
+#include "libdat/array.h"
+#include "libdat/validity.h"
+#include "libga/ga.h"
+#include "libmath/angle.h"
+
+#include <array>
+#include <string>
+#include <sstream>
+
+
+namespace tri
+{
+	using Vec2D = std::array<double, 2u>;
+
+	//! Unit magnitude vector (or null)
+	inline
+	Vec2D
+	unit
+		( Vec2D const & vec
+		)
+	{
+		Vec2D dir{{ dat::nullValue<double>(), dat::nullValue<double>() }};
+		using dat::operator*;
+		double const mag{ dat::magnitude(vec) };
+		if (std::numeric_limits<double>::min() < mag)
+		{
+			dir = (1. / mag) * vec;
+		}
+		return dir;
+	}
+
+//! Individual tiles within tritille tessellation
+namespace tile
+{
+
+	//! Representation of tritille geometry;
+	struct TileGeo
+	{
+		// e.g. rows of transition matrix
+		Vec2D theBarU{{ dat::nullValue<double>(), dat::nullValue<double>() }};
+		Vec2D theBarV{{ dat::nullValue<double>(), dat::nullValue<double>() }};
+
+		//! Construct with null values
+		TileGeo
+			() = default;
+
+		//! Construct tessellation via defining parameters
+		explicit
+		TileGeo
+			( double const & da
+			, double const & db
+			, Vec2D const & avec //!< non-zero vector
+		//	, Vec2D uv0 // -- herein assumed to be {0.,0.}
+			)
+			: TileGeo()
+		{
+			using dat::operator*;
+			using dat::operator-;
+			using dat::operator+;
+
+			Vec2D const adir(unit(avec));
+
+			Vec2D const bdir{{ -adir[1], adir[0] }};
+			Vec2D const uvec(da * adir - db * bdir);
+			Vec2D const udir(unit(uvec));
+
+			Vec2D const vvec(da * adir + db * bdir);
+			Vec2D const vdir(unit(vvec));
+
+			double const gamma{ dat::dot(udir, vdir) };
+
+			double const tmp{ 1. - math::sq(gamma) };
+			if (std::numeric_limits<double>::min() < tmp)
+			{
+				double const inv{ 1./tmp };
+				double const & alpha = inv;
+				double const beta{ -inv * gamma };
+				Vec2D const ubar(alpha*vdir + beta*udir);
+				Vec2D const vbar(alpha*udir + beta*vdir);
+
+				theBarU[0] = ubar[0];
+				theBarU[1] = ubar[1];
+				theBarV[0] = vbar[0];
+				theBarV[1] = vbar[1];
+			}
+		}
+
+		//! Projection of xrel onto udir axis
+		inline
+		double
+		mu
+			( Vec2D const & xrel //!< location relative to tile origin
+			) const
+		{
+			return dat::dot(theBarU, xrel);
+		}
+
+		//! Projection of xrel onto vdir axis
+		inline
+		double
+		nu
+			( Vec2D const & xrel //!< location relative to tile origin
+			) const
+		{
+			return dat::dot(theBarV, xrel);
+		}
+	};
+
+	//! Tessellation indices and weight associated with the (i,j)-th node
+	struct Vertex
+	{
+		size_t theI{ dat::nullValue<size_t>() };
+		size_t theJ{ dat::nullValue<size_t>() };
+		double theW{ dat::nullValue<double>() };
+	};
+
+	//! A triple of (weighted) nodes within the tessellation
+	struct Triangle
+	{
+		std::array<Vertex, 3u> theVerts{{}};
+
+		/*! Interpolate value from a collection of properties
+		 *
+		 * PropType: Must support
+		 *   op: double * PropType
+		 *   op: PropType + PropType
+		 *
+		 * PropGrid: Must support access via
+		 *   PropType = PropGrid(size_t, size_t)
+		*/
+		template <typename PropGrid>
+		inline
+		typename PropGrid::value_type
+		valueFrom
+			( PropGrid const & propGrid
+			) const
+		{
+			size_t const & i1 = theVerts[0].theI;
+			size_t const & i2 = theVerts[1].theI;
+			size_t const & i3 = theVerts[2].theI;
+
+			size_t const & j1 = theVerts[0].theJ;
+			size_t const & j2 = theVerts[1].theJ;
+			size_t const & j3 = theVerts[2].theJ;
+
+			double const & w1 = theVerts[0].theW;
+			double const & w2 = theVerts[1].theW;
+			double const & w3 = theVerts[2].theW;
+
+			assert(i1 < propGrid.high());
+			assert(i2 < propGrid.high());
+			assert(i3 < propGrid.high());
+			assert(j1 < propGrid.wide());
+			assert(j2 < propGrid.wide());
+			assert(j3 < propGrid.wide());
+
+			return
+				{ w1 * propGrid(i1, j1)
+				+ w2 * propGrid(i2, j2)
+				+ w3 * propGrid(i3, j3)
+				};
+		}
+	};
+
+	//! Transformation into tile coordinates
+	struct Finder
+	{
+		double theDelta;
+
+		//! Integer and fractional remainder associated with (value/delta)
+		static
+		inline
+		std::pair<size_t, double>
+		ndxFrac
+			( double const & value
+			, double const & delta
+			)
+		{
+			// force rounding downward (e.g. unlike std::div)
+			double const base{ std::floor(value / delta) };
+			double const frac{ value - base };
+			return {static_cast<size_t>(base), frac };
+		}
+
+		explicit
+		Finder
+			( double const & delta
+			)
+			: theDelta{ delta }
+		{ 
+		}
+
+		tile::Triangle
+		triangleFor
+			( double const & mu
+			, double const & nu
+			) const
+		{
+			tile::Triangle tri;
+
+			std::pair<size_t, double> const muNdxFrac{ ndxFrac(mu, theDelta) };
+			std::pair<size_t, double> const nuNdxFrac{ ndxFrac(nu, theDelta) };
+
+			size_t const & ndxI = muNdxFrac.first;
+			size_t const & ndxJ = nuNdxFrac.first;
+			double const & muFrac = muNdxFrac.second;
+			double const & nuFrac = nuNdxFrac.second;
+
+			if (muFrac < nuFrac)
+			{
+				// use triangle Tv
+				tri.theVerts[0].theI = ndxI;
+				tri.theVerts[0].theJ = ndxJ;
+				tri.theVerts[0].theW = 1. - nuFrac;
+
+				tri.theVerts[1].theI = ndxI + 1u;
+				tri.theVerts[1].theJ = ndxJ + 1u;
+				tri.theVerts[1].theW = muFrac;
+
+				tri.theVerts[2].theI = ndxI;
+				tri.theVerts[2].theJ = ndxJ + 1u;
+				tri.theVerts[2].theW = nuFrac - muFrac;
+			}
+			else
+			{
+				// use triangle Tu
+				tri.theVerts[0].theI = ndxI;
+				tri.theVerts[0].theJ = ndxJ;
+				tri.theVerts[0].theW = 1. - muFrac;
+
+				tri.theVerts[1].theI = ndxI + 1u;
+				tri.theVerts[1].theJ = ndxJ;
+				tri.theVerts[1].theW = muFrac - nuFrac;
+
+				tri.theVerts[2].theI = ndxI + 1u;
+				tri.theVerts[2].theJ = ndxJ + 1u;
+				tri.theVerts[2].theW = nuFrac;
+			}
+
+			return tri;
+		}
+
+	};
+
+} // tile
+
+
+/*! \brief Iso-tritille interpolation entity.
+
+\par Example
+\dontinclude testtri/uIsoTille.cpp
+\skip ExampleStart
+\until ExampleEnd
+*/
+template <typename PropGrid>
+class IsoTille
+{
+	tile::TileGeo theGeo;
+	tile::Finder theFinder;
+	PropGrid const * const thePropGrid;
+
+public: // methods
+
+	//! default null constructor
+	IsoTille
+		() = default;
+
+	//! Construct tessellation of specified geometry
+	explicit
+	IsoTille
+		( double const & da
+		, double const & db
+		, Vec2D const & adir
+		, PropGrid const * const & propGrid
+		)
+		: theGeo(da, db, adir)
+		, theFinder(std::hypot(da, db))
+		, thePropGrid{ propGrid }
+	{
+	}
+
+	//! True if instance is valid
+	bool
+	isValid
+		() const
+	{
+		return false;
+	}
+
+	//! Perform interpolation at xrel
+	typename PropGrid::value_type
+	operator()
+		( Vec2D const & xrel //!< location relative to tile origin
+		) const
+	{
+		double const mu{ theGeo.mu(xrel) };
+		double const nu{ theGeo.nu(xrel) };
+
+		tile::Triangle const tri{ theFinder.triangleFor(mu, nu) };
+
+		return tri.valueFrom<PropGrid>(*thePropGrid);
+	}
+
+	//! Descriptive information about this instance.
+	std::string
+	infoString
+		( std::string const & title = std::string()
+//		, std::string const & fmt = std::string("%12.6f")
+		) const
+	{
+		std::ostringstream oss;
+		if (! title.empty())
+		{
+			oss << title << std::endl;
+		}
+		if (isValid())
+		{
+		}
+		else
+		{
+			oss << " <null>";
+		}
+		return oss.str();
+	}
+
+}; // IsoTille
+
+} // tri
+
+// Inline definitions
+// #include "libtri/IsoTille.inl"
+
+#endif // tri_IsoTille_INCL_
+
---
+++ testtri/demoSphere.cpp
@@ -0,0 +1,242 @@
+//
+//
+// MIT License
+//
+// Copyright (c) 2017 Stellacore Corporation.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject
+// to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+// KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+// AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//
+
+/*! \file
+\brief  This file contains main application program demoSphere
+*/
+
+
+// #include "libdat/info.h"
+#include "libio/stream.h"
+
+#include "libdat/grid.h"
+#include "libgeo/sphere.h"
+#include "libmath/interp.h"
+#include "libmath/Partition.h"
+#include "libprob/pdf.h"
+#include "libtri/IsoTille.h"
+
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <random>
+
+
+namespace
+{
+	//! A pseudo-random direction
+	ga::Vector
+	randomDir
+		()
+	{
+		ga::Vector dir;
+		static std::mt19937_64 rgen;
+		static std::uniform_real_distribution<double> rdistro(-1., 1.);
+		ga::Vector vec;
+		do
+		{
+			vec[0] = rdistro(rgen);
+			vec[1] = rdistro(rgen);
+			vec[2] = rdistro(rgen);
+		}
+		while (1. < ga::magnitude(vec));
+		dir = ga::unit(vec);
+		assert(dat::isValid(dir));
+		return dir;
+	}
+
+	//! Collection of pseudo-random directions
+	std::vector<ga::Vector>
+	randomDirs
+		( size_t const & numDirs = { 64u * 1024u }
+		)
+	{
+		std::vector<ga::Vector> dirs;
+		dirs.reserve(numDirs);
+		while (dirs.size() < numDirs)
+		{
+			dirs.emplace_back(randomDir());
+		}
+		return dirs;
+	}
+
+	//! Example 
+	struct Assessor
+	{
+		std::string const theName;
+		size_t const theSize;
+		std::vector<size_t> theErrCounts;
+		size_t theErrSum;
+		math::Partition const theErrPart;
+
+		//! Construct named quality tracker
+		Assessor
+			( std::string const & name
+			)
+			: theName(name)
+			, theSize{ 256u }
+			, theErrCounts(theSize, 0u)
+			, theErrSum{ 0u }
+			, theErrPart(0., 1./(double(theSize)), theSize)
+		{ }
+
+		//! Incorporate observation error into quality assessment
+		void
+		addSample
+			( double const & err
+			)
+		{
+			if (dat::isValid(err))
+			{
+				size_t const bin{ theErrPart.binIndexFor(err) };
+				assert(bin < theErrCounts.size());
+				theErrCounts[bin] += 1.;
+				theErrSum += 1.;
+			}
+		}
+
+		//! Probability of requested error
+		double
+		probabilityAt
+			( double const & errValue //!< [0,1]
+			) const
+		{
+			double prob{ dat::nullValue<double>() };
+			assert(0. <= errValue);
+			assert(errValue <= 1.);
+			double const subIndex{ errValue * double(theErrPart.size()) };
+			double const count
+				{ math::interp::linear<double>(subIndex, theErrCounts) };
+			if (dat::isValid(count))
+			{
+				prob = count / theErrSum;
+			}
+			return prob;
+		}
+	};
+
+	// write gnuplot data
+	void
+	saveForGnuplot
+		( std::vector<Assessor> const & quals
+		)
+	{
+		std::string datname("errSphere.dat");
+		std::string pltname("errSphere.plt");
+		std::ofstream ofsdat(datname);
+		std::ofstream ofsplt(pltname);
+
+		// .plt file
+		ofsplt << "plot";
+		size_t col{2u};
+		for (Assessor const & qual : quals)
+		{
+			if (2u == col)
+			{
+				ofsplt << " ";
+			}
+			else
+			{
+				ofsplt << ",";
+			}
+			ofsplt
+				<< "'" << datname << "' u 1:" << col << " w l"
+				<< " ti '" << qual.theName << "'";
+			++col;
+		}
+		ofsplt << '\n';
+
+		// .dat file
+		for (double err{0.} ; err <= 1. ; err += 1./256.)
+		{
+			ofsdat << dat::infoString(err);
+			for (Assessor const & qual : quals)
+			{
+				ofsdat << " " << dat::infoString(qual.probabilityAt(err));
+			}
+			ofsdat << '\n';
+		}
+		ofsplt << std::endl;
+		ofsdat << std::endl;
+	}
+
+}
+
+//! Demonstrate iso-tritille tessellation of a unit sphere
+int
+main
+	()
+{
+	using PropType = double;
+	using PairZA = tri::Vec2D;
+
+	// get (pseudo)random sampling directions
+	std::vector<ga::Vector> const dirs{ randomDirs(32u) };
+
+	// various test cases
+	std::vector<Assessor> quals
+	{
+		  Assessor("cfg1")
+		, Assessor("cfg2")
+		, Assessor("cfg3")
+		, Assessor("cfg4")
+		, Assessor("cfg5")
+	};
+
+	using PropGrid = dat::grid<PropType>;
+	PropGrid const radGrid;
+
+	// for each case
+	for (Assessor & qual : quals)
+	{
+		double const da{ 1. };
+		double const db{ 1. };
+		PairZA const adir{ 1., 0. };
+		tri::IsoTille<PropGrid> const tin(da, db, adir, &radGrid);
+
+		// compute histogram of interpolation errors
+		for (ga::Vector const & dir : dirs)
+		{
+			using namespace geo::sphere;
+			PairZA const zaLoc{ zenithOf(dir), azimuthOf(dir) };
+			PropType const gotRad{ tin(zaLoc) };
+			if (dat::isValid(gotRad))
+			{
+				constexpr PropType expRad{ 1. };
+				PropType const err{ gotRad - expRad };
+				qual.addSample(err);
+			}
+		}
+
+	}
+
+	// write data to file for display
+	saveForGnuplot(quals);
+
+	return 0;
+}

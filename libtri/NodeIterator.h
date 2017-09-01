45b335d906c702acf38382cf50a10425c96c2f54
transpixel <dave@transpixel.org>
2017-09-01 13:12:41 -0600

    libtri/NodeIterator iterator over tritille nodes

---
+++ libtri/NodeIterator.h
@@ -0,0 +1,230 @@
+//
+//
+// MIT License
+//
+// Copyright (c) 2017 Stellacore Corporation.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject
+// to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+// KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+// AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//
+
+#ifndef tri_NodeIterator_INCL_
+#define tri_NodeIterator_INCL_
+
+/*! \file
+\brief Declarations for tri::NodeIterator
+*/
+
+
+#include "libdat/Area.h"
+#include "libdat/validity.h"
+#include "libtri/IsoGeo.h"
+
+#include <string>
+#include <sstream>
+
+
+namespace tri
+{
+
+/*! \brief Iterator over tritille nodes within valid domain.
+
+\par Example
+\dontinclude testtri/uNodeIterator.cpp
+\skip ExampleStart
+\until ExampleEnd
+*/
+
+class NodeIterator
+{
+
+	IsoGeo theGeo{};
+	dat::Area<double> theDomainTest{}; // TODO replace with functor?
+
+	std::pair<long, long> theMinMaxMu
+		{ dat::nullValue<long>(), dat::nullValue<long>() };
+	std::pair<long, long> theMinMaxNu
+		{ dat::nullValue<long>(), dat::nullValue<long>() };
+
+	std::pair<long, long> theAtMuNu
+		{ dat::nullValue<long>(), dat::nullValue<long>() };
+	bool theIsActive{ false };
+
+public: // methods
+
+	//! default null constructor
+	NodeIterator
+		() = default;
+
+	//! Construct iterator to cover domain
+	explicit
+	NodeIterator
+		( IsoGeo const & trigeo
+		, dat::Area<double> const & xyDomain // TODO replace with functor?
+	//	, dat::Area<double> const & mnRange
+		)
+		: NodeIterator()
+	{
+		dat::Area<double> const mnRange{ trigeo.mnArea(xyDomain) };
+
+		if (mnRange.isValid())
+		{
+			long const muMin{ static_cast<long>(mnRange[0].min()) };
+			long const muMax{ static_cast<long>(mnRange[0].max()) };
+			long const nuMin{ static_cast<long>(mnRange[1].min()) };
+			long const nuMax{ static_cast<long>(mnRange[1].max()) };
+			assert (muMin <= muMax);
+			assert (nuMin <= nuMax);
+
+			theGeo = trigeo;
+			theDomainTest = xyDomain;
+			theMinMaxMu = { muMin, muMax };
+			theMinMaxNu = { nuMin, nuMax };
+			theAtMuNu = { theMinMaxMu.first, theMinMaxNu.first };
+			theIsActive = true; // assume unless contradicted
+			if (! atNodeIsValid())
+			{
+				advanceToNextValid();
+			}
+		}
+	}
+
+	//! True if iterator is currently active (not at end)
+	inline
+	explicit
+	operator bool
+		() const
+	{
+		return theIsActive;
+	}
+
+	//! Index pair associated with current (valid) iterator
+	inline
+	std::pair<long, long>
+	operator*
+		() const
+	{
+		return theAtMuNu;
+	}
+
+	//! Advance to next valid node (node in the domain)
+	inline
+	NodeIterator &
+	operator++
+		()
+	{
+		advanceToNextValid();
+		return *this;
+	}
+
+	//! Descriptive information about this instance.
+	std::string
+	infoString
+		( std::string const & title = std::string()
+		) const
+	{
+		std::ostringstream oss;
+		if (! title.empty())
+		{
+			oss << title << std::endl;
+		}
+
+		// oss << theGeo.infoString("theGeo");
+		// oss << std::endl;
+
+		oss << "muMinMax: " << dat::infoString(theMinMaxMu);
+
+		oss << std::endl;
+		oss << "nuMinMax: " << dat::infoString(theMinMaxNu);
+
+		oss << std::endl;
+		if (dat::isValid(theAtMuNu))
+		{
+			oss << "atMuNu: " << dat::infoString(theAtMuNu);
+		}
+		else
+		{
+			oss << "theAtMuNu: " << "<null>";
+		}
+
+		oss << std::endl;
+		oss << "active: " << dat::infoString(theIsActive);
+
+		return oss.str();
+	}
+
+private:
+
+	//! True if xyLoc is within domain area
+	inline
+	bool
+	atNodeIsValid
+		() const
+	{
+		dat::Spot const mnSpot
+			{{ double(theAtMuNu.first), double(theAtMuNu.second) }};
+		return theDomainTest.contains(theGeo.xyRelative(mnSpot));
+	}
+
+	//! Increment theAtMuNu to next node (whether valid or not)
+	inline
+	void
+	advanceToNextAny
+		()
+	{
+		long & ndxAtMu = theAtMuNu.first;
+		long & ndxAtNu = theAtMuNu.second;
+		++ndxAtNu;
+		if (theMinMaxNu.second < ndxAtNu)
+		{
+			ndxAtNu = theMinMaxNu.first;
+			++ndxAtMu;
+			if (theMinMaxMu.second < ndxAtMu)
+			{
+				theIsActive = false;
+				ndxAtMu = dat::nullValue<long>();
+				ndxAtNu = dat::nullValue<long>();
+			}
+		}
+	}
+
+	//! Increment iterator until pointing to next valid instance (or end)
+	inline
+	void
+	advanceToNextValid
+		()
+	{
+		do
+		{
+			advanceToNextAny();
+		}
+		while (theIsActive && (! atNodeIsValid()));
+	}
+
+}; // NodeIterator
+
+} // tri
+
+// Inline definitions
+// #include "libtri/NodeIterator.inl"
+
+#endif // tri_NodeIterator_INCL_
+
---
+++ testtri/uNodeIterator.cpp
@@ -0,0 +1,175 @@
+//
+//
+// MIT License
+//
+// Copyright (c) 2017 Stellacore Corporation.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject
+// to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+// KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+// AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//
+
+/*! \file
+\brief  This file contains unit test for tri::NodeIterator
+*/
+
+
+#include "libtri/NodeIterator.h"
+
+#include "libdat/info.h"
+#include "libdat/validity.h"
+#include "libio/stream.h"
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <set>
+#include <sstream>
+#include <string>
+
+
+namespace
+{
+
+//! Check for common functions
+std::string
+tri_NodeIterator_test0
+	()
+{
+	std::ostringstream oss;
+	/*
+	tri::NodeIterator const aNull;
+	if (dat::isValid(aNull))
+	{
+		oss << "Failure of null value test" << std::endl;
+	//	oss << "infoString: " << dat::infoString(aNull) << std::endl;
+	}
+	*/
+	return oss.str();
+}
+
+//! Check basic iterator operations
+std::string
+tri_NodeIterator_test1
+	()
+{
+	std::ostringstream oss;
+
+	dat::Range<double> xRange{ -10., 10. };
+	dat::Range<double> yRange{ -20., 20. };
+	dat::Area<double> xyDomain{ xRange, yRange };
+
+	constexpr std::array<double, 2u> aDir{{ 0., 1. }};
+	tri::IsoGeo const trigeo( 5., 7., aDir);
+
+	// determine acceptable iterator
+	std::set<std::pair<long, long> > expMuNus;
+	{
+		dat::Area<double> const rngArea{ trigeo.mnArea(xyDomain) };
+		long const muBeg{ (long)std::floor(rngArea[0].min()) };
+		long const muEnd{ (long)std::ceil(rngArea[0].max()) };
+		long const nuBeg{ (long)std::floor(rngArea[1].min()) };
+		long const nuEnd{ (long)std::ceil(rngArea[1].max()) };
+		for (long ndxNu{nuBeg} ; ndxNu < nuEnd ; ++ndxNu)
+		{
+			for (long ndxMu{muBeg} ; ndxMu < muEnd ; ++ndxMu)
+			{
+				dat::Spot const mnLoc{{ double(ndxMu), double(ndxNu) }};
+				dat::Spot const xyLoc(trigeo.xyRelative(mnLoc));
+				if (xyDomain.contains(xyLoc))
+				{
+					expMuNus.insert(std::make_pair(ndxMu, ndxNu));
+				}
+			}
+		}
+	}
+	assert(! expMuNus.empty());
+
+	std::set<std::pair<long, long> > gotMuNus;
+	for (tri::NodeIterator iter(trigeo, xyDomain) ; iter ; ++iter)
+	{
+		gotMuNus.insert(*iter);
+	}
+
+	/*
+	std::ofstream ofsExp("nodeIter_exp.dat");
+	for (std::pair<long, long> const & expMuNu : expMuNus)
+	{
+		ofsExp << dat::infoString(expMuNu, "expMuNu") << std::endl;
+	}
+	std::ofstream ofsGot("nodeIter_got.dat");
+	for (std::pair<long, long> const & gotMuNu : gotMuNus)
+	{
+		ofsGot << dat::infoString(gotMuNu, "gotMuNu") << std::endl;
+	}
+	*/
+
+	// check sizes
+	if (gotMuNus.size() != expMuNus.size())
+	{
+		oss << "Failure of size test" << std::endl;
+		oss << dat::infoString(expMuNus.size(), "expSize") << std::endl;
+		oss << dat::infoString(gotMuNus.size(), "gotSize") << std::endl;
+	}
+
+	// check for same contents
+	std::set<std::pair<long, long> > difMuNus;
+	std::set_difference
+		( gotMuNus.begin(), gotMuNus.end()
+		, expMuNus.begin(), expMuNus.end()
+		, std::inserter(difMuNus, difMuNus.end())
+		);
+	if (! difMuNus.empty())
+	{
+		oss << "Failure of zero-differnce test" << std::endl;
+		for (std::pair<long, long> const & difMuNu : difMuNus)
+		{
+			oss << dat::infoString(difMuNu, "difMuNu") << std::endl;
+		}
+	}
+
+	return oss.str();
+}
+
+
+}
+
+//! Unit test for tri::NodeIterator
+int
+main
+	( int const /*argc*/
+	, char const * const * /*argv*/
+	)
+{
+	std::ostringstream oss;
+
+	// run tests
+	oss << tri_NodeIterator_test0();
+	oss << tri_NodeIterator_test1();
+
+	// check/report results
+	std::string const errMessages(oss.str());
+	if (! errMessages.empty())
+	{
+		io::err() << errMessages << std::endl;
+		return 1;
+	}
+	return 0;
+}

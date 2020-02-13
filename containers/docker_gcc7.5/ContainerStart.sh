67baaf66f80d3789dbdc6c6d3c2ba70f276469e5
Dave Knopp <dave@stellacore.com>
2020-02-13 10:19:32 -0700

    containers/docker_gcc7.5 added

---
+++ containers/docker_gcc7.5/ContainerStart.sh
@@ -0,0 +1,8 @@
+# Start image as container
+
+docker run --rm -t -i -d -P \
+	--mount="type=bind,source=${HOME}/repos/tpqz,destination=/tpqz" \
+	--name=tpqzBuild_gcc-latest \
+	--hostname=tpqzBuild \
+	transpixel/tpqz:gcc7.5
+
